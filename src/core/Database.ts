c3df46cccbb085d065c37bc62ae57cfcaf5c6691
Joe Zaba <joseph.zaba@outlook.com>
2021-01-16 15:35:00 -0700

    changed Datebase.ts

--- src/core/Database.ts
+++ src/core/Database.ts
@@ -11,8 +11,8 @@ export default new class Database {
             password: process.env.DB_PASSWORD || "",
             database: process.env.DB_NAME || "database",
             port: process.env.DB_PORT ? +process.env.DB_PORT : 3306,
-            sslmode: 'REQUIRED',
-            logging: false
+            'ssl-mode': 'REQUIRED',
+            //logging: false
         }
 
     }
@@ -29,9 +29,9 @@ export default new class Database {
     }
 
     public getConnection(): mysql.Connection {
-        if(process.env.DATABASE_URL){
-         return mysql.createConnection(process.env.DATABASE_URL);
-        }
+        // if(process.env.DATABASE_URL){
+        //  return mysql.createConnection(process.env.DATABASE_URL);
+        // }
         return mysql.createConnection(this.dbConfig()); 
     }
 
